2f50078c4379836aadc9d274fdc5eeeef85e2523
Pat <pat.mccartney2@gmail.com>
2018-08-23 13:24:03 -0600

    working articles index

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -6,8 +6,9 @@ describe "user sees all articles" do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
       article_2 = Article.create!(title: "Title 2", body: "Body 2")
       visit '/articles'
-      expect(page).to have_content(article_1.title)
+      expect(page).to have_link(article_1.title)
       expect(page).to have_content(article_2.title)
+      expect(page).to have_link('Create a New Article')
 
     end
   end
